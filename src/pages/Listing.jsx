9e75d665b6691d3d378622126e87f0d41735b803
m-honarzade <honarzadeh.mahdie@gmail.com>
2023-07-26 12:38:44 +0430

    add the listing information to the listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -5,7 +5,14 @@ import { db } from "../firebase";
 
 import Spinner from "../components/Spinner";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { FaShare } from "react-icons/fa";
+import {
+  FaShare,
+  FaMapMarkerAlt,
+  FaBed,
+  FaBath,
+  FaParking,
+  FaChair,
+} from "react-icons/fa";
 
 import SwiperCore, {
   EffectFade,
@@ -75,6 +82,63 @@ const Listing = () => {
           Link Copied.
         </p>
       )}
+      <div className=" flex flex-col md:flex-row m-4 max-w-6xl lg:mx-auto rounded-lg bg-white p-4 lg:space-x-5 shadow-lg">
+        <div className=" w-full h-[200px] lg:h-[400px]">
+          <p className="text-2xl font-bold mb-3 text-blue-900">
+            {listing.name} - ${" "}
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            {listing.type === "rent" ? "/ month" : ""}
+          </p>
+          <p className="flex items-center mt-6 mb-3 font-semibold ">
+            <FaMapMarkerAlt className="text-green-700 mr-1" />
+            {listing.address}
+          </p>
+          <div className="flex  w-[%75] justify-start items-center space-x-4">
+            <p className="bg-red-800 text-white w-full max-w-[200px] rounded shadow-md font-semibold p-1 text-center">
+              {listing.type === "rent" ? "for Rent" : "for Sale"}
+            </p>
+            <p className="bg-green-800 text-white w-full max-w-[200px] rounded shadow-md font-semibold p-1 text-center">
+              {listing.offer && (
+                <p>
+                  {(listing.regularPrice - listing.discountedPrice)
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+                  discount
+                </p>
+              )}
+            </p>
+          </div>
+          <p className="mt-3 mb-3">
+            <span className="font-semibold ">Description - </span>
+            {listing.description}
+          </p>
+          <ul className="flex flex-row space-x-2 items-center lg:space-x-10 text-sm font-semibold">
+            <li className="flex items-center whitespace-nowrap">
+              <FaBed className="text-lg mr-1" />
+              {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+              <FaBath className="text-lg mr-1" />
+              {+listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+              <FaParking className="text-lg mr-1" />
+              {listing.parking ? `Parking spot` : "No parking"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+              <FaChair className="text-lg mr-1" />
+              {listing.furnished ? `Furnished` : "Not furnished"}
+            </li>
+          </ul>
+        </div>
+        <div className=" w-full h-[200px] lg:h-[400px] z-10 overflow-x-hidden "></div>
+      </div>
     </main>
   );
 };
