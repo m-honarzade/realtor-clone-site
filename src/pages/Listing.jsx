df82141f2c4c02cd425fe3d2005bb18fe6772060
m-honarzade <honarzadeh.mahdie@gmail.com>
2023-07-27 14:51:57 +0430

    add map to listing page using leaflet

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -22,6 +22,7 @@ import SwiperCore, {
 } from "swiper";
 import "swiper/css/bundle";
 import Contact from "../components/Contact";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const Listing = () => {
   const auth = getAuth();
@@ -154,7 +155,24 @@ const Listing = () => {
             <Contact userRef={listing.useRef} listing={listing} />
           )}
         </div>
-        <div className=" w-full h-[200px] lg:h-[400px] z-10 overflow-x-hidden "></div>
+        <div className=" w-full h-[200px] md:h-[380px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2">
+          <MapContainer
+            center={[listing.geoLocation.lat, listing.geoLocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={[listing.geoLocation.lat, listing.geoLocation.lng]}
+            >
+              <Popup>{listing.address}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
       </div>
     </main>
   );
